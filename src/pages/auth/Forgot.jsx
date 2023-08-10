f20e27cf2a5e2c9ba217de9dd274e665c378b4a4
Rufi-git <rufialiyev2005@gmail.com>
2023-08-10 03:58:39 -0700

    sadasdasd

--- src/pages/auth/Forgot.jsx
+++ src/pages/auth/Forgot.jsx
@@ -33,8 +33,8 @@ const Forgot = () => {
     }
 
     return (
-        <div className="sm:bg-[#f8f8f8] h-screen flex items-center">
-            <div className="flex lg:flex-row flex-col items-center gap-14 mx-auto px-[50px] py-[70px] sm:shadow-md rounded-md max-w-[1240px] bg-white p-5">
+        <div className="bg-[#f8f8f8] h-screen flex items-center">
+            <div className="sm:flex lg:flex-row flex-col items-center gap-14 mx-auto px-[100px] py-[70px] shadow-md rounded-md max-w-[1240px] bg-white p-5">
                 <div className="lg:max-w-[350px] sm:max-w-[250px] hidden sm:block">
                     <img src={forgot} alt="" className="w-full" />
                 </div>
