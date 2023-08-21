6c3fc688bf5b7543ef21fdfa6771ecf0623e2a30
Rufi-git <rufialiyev2005@gmail.com>
2023-08-21 07:52:20 -0700

    123

--- src/pages/admin/QuestionAdd.jsx
+++ src/pages/admin/QuestionAdd.jsx
@@ -1,91 +1,87 @@
 import React, { useEffect, useState } from 'react';
-import { HiUsers } from 'react-icons/hi';
-import { BiSolidUserCheck, BiUserMinus, BiUserX } from 'react-icons/bi';
-import { AiFillDelete } from 'react-icons/ai';
-import PageMenu from '../../components/PageMenu';
-import Categories from '../../components/Categories';
-import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
 import { useDispatch, useSelector } from 'react-redux';
-import { getTags, addExam, getTag, addQuestion, getQuestionByExam } from '../../../redux/features/quiz/quizSlice';
-import { toast } from 'react-toastify';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Modal from 'react-modal';
-import QuestionList from '../../components/QuestionList';
+import { toast } from 'react-toastify';
 import Spinner from '../../components/Spinner';
-
+import QuestionList from '../../components/QuestionList';
+import { getQuestionByExam, addQuestion } from '../../../redux/features/quiz/quizSlice';
+import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
 
 const QuestionAdd = () => {
     useRedirectLoggedOutUser('/login');
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
-    const { isLoading, isSuccess, isError, queue } = useSelector(state => state.quiz)
-    const [modalIsOpen, setIsOpen] = useState(false);
-
-    function openModal() {
-        setIsOpen(true);
-    }
-
-    function closeModal() {
-        setIsOpen(false);
-    }
-
-    const { examId } = useParams()
+    const { examId } = useParams();
     const initialState = {
         name: "",
-        option1: "",
-        option2: "",
-        option3: "",
-        option4: "",
-    }
-    const [questionForm, setQuestionForm] = useState(initialState)
-    const { name, option1, option2, option3, option4 } = questionForm
+        options: [{ text: "", isCorrect: true }],
+    };
+    const [questionForm, setQuestionForm] = useState(initialState);
+    const [selectedCorrectOption, setSelectedCorrectOption] = useState(0);
 
-    const [selectedCorrectOption, setSelectedCorrectOption] = useState('option1');
+    const { name, options } = questionForm;
 
-    const handleOptionChange = (e) => {
-        setSelectedCorrectOption(e.target.name);
+    const handleOptionChange = (e, index) => {
+        const updatedOptions = [...options];
+        updatedOptions[index].text = e.target.value;
+        setQuestionForm({ ...questionForm, options: updatedOptions });
+    };
+
+    const handleCorrectOptionChange = (index) => {
+        setSelectedCorrectOption(index);
+    };
+
+    const addNewOption = () => {
+        const newOptions = [...options, { text: "", isCorrect: false }];
+        setQuestionForm({ ...questionForm, options: newOptions });
     };
 
     const handleInputChange = (e) => {
-        const { name, value } = e.target
-        setQuestionForm({ ...questionForm, [name]: value })
-    }
-
-    useEffect(() => {
-        dispatch(getQuestionByExam(examId))
-    }, [dispatch])
+        const { name, value } = e.target;
+        setQuestionForm({ ...questionForm, [name]: value });
+    };
 
     const resetForm = () => {
         setQuestionForm(initialState);
-        setSelectedCorrectOption('option1');
+        setSelectedCorrectOption(0);
     };
 
-
     const addExamForm = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
         const questionData = {
             name,
-            options: [
-                { text: option1, isCorrect: selectedCorrectOption === 'option1' },
-                { text: option2, isCorrect: selectedCorrectOption === 'option2' },
-                { text: option3, isCorrect: selectedCorrectOption === 'option3' },
-                { text: option4, isCorrect: selectedCorrectOption === 'option4' },
-            ],
-        }
+            options: options.map((option, index) => ({
+                text: option.text,
+                isCorrect: index === selectedCorrectOption,
+            })),
+        };
 
-        if (name && option1 && option2 && option3 && option4) {
-            const addQuestionData = await dispatch(addQuestion({ examId, questionData }))
+        if (name && options.every(option => option.text !== "")) {
+            const addQuestionData = await dispatch(addQuestion({ examId, questionData }));
 
-            if (addQuestionData.type != "quiz/addExam/rejected") {
-                closeModal()
-                dispatch(getQuestionByExam(examId))
-                resetForm()
+            if (addQuestionData.type !== "quiz/addExam/rejected") {
+                closeModal();
+                dispatch(getQuestionByExam(examId));
+                resetForm();
             }
         } else {
-            toast.error("All fields are required")
+            toast.error("All fields are required");
         }
-    }
+    };
+
+    const { isLoading } = useSelector(state => state.quiz);
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    const openModal = () => {
+        setIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsOpen(false);
+    };
+
     return (
         <div className="bg-gray-50 max-w-[1440px] mx-auto p-10 my-5 relative">
             <div className='relative h-[700px] overflow-y-auto flex justify-between flex-col'>
@@ -93,20 +89,19 @@ const QuestionAdd = () => {
                 <QuestionList />
             </div>
             <Modal
-                className={"z-[10000] max-w-[1200px] px-4"}
+                className={"z-[10000] max-w-[1200px] px-4 overflow-y-auto max-h-[50%]"}
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
                 contentLabel="Example Modal"
                 style={{
                     content: {
-                        position:"relative",
+                        position: "relative",
                         top: '50%',
                         left: '50%',
                         transform: 'translate(-50%, -50%)',
                     },
                 }}
             >
-
                 <div className="w-full max-w-[1240px] bg-white p-8 rounded-md shadow-md">
                     <form onSubmit={addExamForm}>
                         <div className="mb-4">
@@ -127,102 +122,43 @@ const QuestionAdd = () => {
                                 Seçimlər:
                             </label>
                             <div className='grid md:grid-cols-2 gap-3'>
-                                <div className='flex items-center'>
-                                    <input
-                                        checked={selectedCorrectOption === 'option1'}
-                                        onChange={handleOptionChange}
-                                        type="radio"
-                                        name="option1"
-                                        id="option1"
-                                        className="hidden mr-2 text-[40px]"
-                                    />
-                                    <label htmlFor='option1' className={`${selectedCorrectOption === 'option1' ? "text-blue-500" : "text-gray-700"} border cursor-pointer bg-gray-50 flex w-full p-2 items-center`}>
+                                {options.map((option, index) => (
+                                    <div className='flex items-center' key={index}>
                                         <input
-                                            value={option1}
-                                            onChange={handleInputChange}
-                                            type="text"
-                                            name='option1'
-                                            id="option1"
-                                            className={`${selectedCorrectOption === 'option1' ? "border-[#1084da] border" : ""} block h-[40px] w-full outline-none border px-2 py-1 shadow-sm`}
+                                            checked={selectedCorrectOption === index}
+                                            onChange={() => handleCorrectOptionChange(index)}
+                                            type="radio"
+                                            name={`correctOption-${index}`}
+                                            id={`correctOption-${index}`}
+                                            className="hidden mr-2 text-[40px]"
                                         />
-                                    </label>
-                                </div>
-                                <div className='flex items-center'>
-                                    <input
-                                        checked={selectedCorrectOption === 'option2'}
-                                        onChange={handleOptionChange}
-                                        type="radio"
-                                        name="option2"
-                                        id="option2"
-                                        className="mr-2 hidden"
-                                    />
-                                    <label htmlFor='option2' className={`${selectedCorrectOption === 'option2' ? "text-blue-500" : "text-gray-700"} border cursor-pointer bg-gray-50 flex w-full p-2 items-center`}>
-                                        <input
-                                            value={option2}
-                                            onChange={handleInputChange}
-                                            type="text"
-                                            name='option2'
-                                            id="option2"
-                                            className={`${selectedCorrectOption === 'option2' ? "border-[#1084da] border" : ""} block h-[40px] w-full outline-none border px-2 py-1 shadow-sm`}
-                                        />
-                                    </label>
-                                </div>
-
-                                <div className='flex items-center'>
-                                    <input
-                                        checked={selectedCorrectOption === 'option3'}
-                                        onChange={handleOptionChange}
-                                        type="radio"
-                                        name="option3"
-                                        id="option3"
-                                        className="mr-2 hidden"
-                                    />
-                                    <label htmlFor='option3' className={`${selectedCorrectOption === 'option3' ? "text-blue-500" : "text-gray-700"} border cursor-pointer bg-gray-50 flex w-full p-2 items-center`}>
-                                        <input
-                                            value={option3}
-                                            onChange={handleInputChange}
-                                            type="text"
-                                            name='option3'
-                                            id="option3"
-                                            className={`${selectedCorrectOption === 'option3' ? "border-[#1084da] border" : ""} block h-[40px] w-full outline-none border px-2 py-1 shadow-sm`}
-                                        />
-                                    </label>
-                                </div>
-
-                                <div className='flex items-center'>
-                                    <input
-                                        checked={selectedCorrectOption === 'option4'}
-                                        onChange={handleOptionChange}
-                                        type="radio"
-                                        name="option4"
-                                        id="option4"
-                                        className="mr-2 hidden"
-                                    />
-                                    <label htmlFor='option4' className={`${selectedCorrectOption === 'option4' ? "text-blue-500" : "text-gray-700"} border cursor-pointer bg-gray-50 flex w-full p-2 items-center`}>
-                                        <input
-                                            value={option4}
-                                            onChange={handleInputChange}
-                                            type="text"
-                                            name='option4'
-                                            id="option4"
-                                            className={`${selectedCorrectOption === 'option4' ? "border-[#1084da] border" : ""} block h-[40px] w-full outline-none border px-2 py-1 shadow-sm`}
-                                        />
-                                    </label>
-                                </div>
+                                        <label htmlFor={`correctOption-${index}`} className={`${selectedCorrectOption === index ? "text-blue-500" : "text-gray-700"} border cursor-pointer bg-gray-50 flex w-full p-2 items-center`}>
+                                            <input
+                                                value={option.text}
+                                                onChange={(e) => handleOptionChange(e, index)}
+                                                type="text"
+                                                name={`option-${index}`}
+                                                id={`option-${index}`}
+                                                className={`${selectedCorrectOption === index ? "border-[#1084da] border" : ""} block h-[40px] w-full outline-none border px-2 py-1 shadow-sm`}
+                                            />
+                                        </label>
+                                    </div>
+                                ))}
                             </div>
+                            <button type='button' onClick={addNewOption} className='bg-[#47b7d3] my-3 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50'>Add Option</button>
                         </div>
-                        {
-                            isLoading ?
-                                <button className="bg-[#6dabe4] w-[120px] flex justify-center text-white py-2 px-4 rounded-md text-sm" disabled><Spinner /></button>
-                                :
-                                <button
-                                    type="submit"
-                                    className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                                >
-                                    Add Question
-                                </button>
-                        }
-
+                        {isLoading ? (
+                            <button className="bg-[#6dabe4] w-[120px] flex justify-center text-white py-2 px-4 rounded-md text-sm" disabled>
+                                <Spinner />
+                            </button>
+                        ) : (
+                            <button
+                                type="submit"
+                                className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                            >
+                                Add Question
+                            </button>
+                        )}
                     </form>
                 </div>
             </Modal>
